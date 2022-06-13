52ee594bc172fa52ea3be724c57e463de4a0e142
Prasanna Adhikari <adhikariprasannaa@gmail.com>
2022-06-13 11:24:43 +0545

    update for glitch

--- Assignment8/index.js
+++ Assignment8/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const port = 8080;
 
 app.use(express.json());
-app.use(cors());
 
 app.use(express.static("./"));
 
